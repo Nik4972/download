9f12eec84bc51456fa3198b489caae0ff2e40abf
Nik4972 <nikon2001@ukr.net>
2018-11-24 23:57:45 +0200

    Update Download.php

--- Download.php
+++ Download.php
@@ -2,10 +2,8 @@
 
 class Download
 {
-
     static public function download($url, $target) 
     {
-
       try {
 
         if (!$hfile = fopen($target, "w")) {
@@ -14,7 +12,6 @@ class Download
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11');
-
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FILE, $hfile);
