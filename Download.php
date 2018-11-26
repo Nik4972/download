d446f97ee163576c786a6490295f8ec04c2ae98f
Nik4972 <nikon2001@ukr.net>
2018-11-26 11:06:53 +0200

    Update Download.php

--- Download.php
+++ Download.php
@@ -14,29 +14,21 @@ class Download
     {
 
       try {
-            // Создаем и открываем файл для записи
-        if (!$hfile = fopen($target, "w")) {
-            throw new Exception('Ошибка создания файла!');
-        }
 
-            // Создаем cURL запрос для загрузки файла из $url
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11');
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FILE, $hfile);
+        // Загружаем данные из файла
+      $data = file_get_contents($url);
 
-        if (!curl_exec($ch)) { // Выполняем запрос. Если неудачный - освобождаем ресурсы, выдаем ошибку.
-            curl_close($ch);
-            fclose($hfile);
-            unlink($target);
-            throw new Exception('Ошибка загрузки файла!');
-        }
-          // Если удачный - сохраняем файл,освобождаем ресурсы, возвращаем true.
-        fflush($hfile);
-        fclose($hfile);
-        curl_close($ch);
+        // Записываем данные в локальный файл 
+      $data = file_put_contents($target, $data);
+
+      if (!$data){
+      
+        return false;
+
+      } else {
+        
         return true;
+      }
 
       } catch (Throwable $t) {
             echo $t->getMessage() . "\n";
@@ -45,4 +37,3 @@ class Download
       }	
     }
 }
-
